a66ba8e0da6c459d783cba6d3f1cd8504af94404
Lchiguer1 <91809708+Lchiguer1@users.noreply.github.com>
2021-10-18 11:42:50 -0500

    showing Parent Systemout

--- src/org/mma/training/java/Override.java
+++ src/org/mma/training/java/Override.java
@@ -5,8 +5,8 @@ public class Override {
 	public static void main(String[] args) {
 		Child Obj = new Child();
 		Obj.display();
-		//Parent Obj1 = new Parent();
-		//Obj1.display();
+		Parent Obj1 = new Parent();
+		Obj1.display();
 
 	}
 
